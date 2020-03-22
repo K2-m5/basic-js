c4646e228bb5c91dfe6ec6421ecab5c03d372209
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-03-22 23:42:20 +0300

    feat: add solution dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeam=[];
+  if (Array.isArray(members)) {
+    for(let member of members) {
+      if( typeof member ==="string") {
+        dreamTeam.push(member.trim()[0].toUpperCase());
+      }
+    }
+    return dreamTeam.sort().join("");
+  } else return false;
 };
